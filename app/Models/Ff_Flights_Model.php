de19f7418b63c8223bb2d8bdcc91a875844d0416
Agne <agnepetrylaite@gmail.com>
2017-08-11 14:45:37 +0300

    collisions

--- app/Models/Ff_Flights_Model.php
+++ app/Models/Ff_Flights_Model.php
@@ -7,4 +7,22 @@ class Ff_Flights_Model extends CoreModel
     protected $table =  'ff_flights';
 
     protected $fillable = ['id', 'arival', 'departure', 'airline_id', 'destination_id', 'origin_id'];
+
+    protected $with = ['originAirport', 'destinationAirport', 'airline'];
+
+    public function originAirport()
+    {
+        return $this->hasOne(Ff_AirPorts_Model::class, 'id', 'origin_id');
+    }
+
+    public function destinationAirport()
+    {
+        return $this->hasOne(Ff_AirPorts_Model::class, 'id', 'destination_id');
+    }
+
+    public function airline()
+    {
+        return $this->hasOne(Ff_AirLines_Model::class, 'id', 'airline_id');
+    }
+
 }
