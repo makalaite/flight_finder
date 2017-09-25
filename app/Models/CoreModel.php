19d517424615d159493832c2a8f1b1f528026597
Agne <agnepetrylaite@gmail.com>
2017-09-26 00:39:13 +0300

    core model uses uuid trait

--- app/Models/CoreModel.php
+++ app/Models/CoreModel.php
@@ -2,15 +2,21 @@
 
 namespace App\Models;
 
+use App\Models\Traits\UuidTrait;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Ramsey\Uuid\Uuid;
 
 class CoreModel extends Model
 {
-    use SoftDeletes;
     public $incrementing = false;
+
+    use SoftDeletes;
+
+    use UuidTrait;
+
     protected $hidden = ['created_at', 'updated_at', 'deleted_at', 'count'];
+
     protected static function boot()
     {
         parent::boot();
